de739802d4f9d7462832eb1f07a8e28a5d13c7d7
Gustavo Gomes <gustxvogomxs@gmail.com>
2023-09-21 22:30:17 -0300

    feat: create model and migration of ToDo

---
+++ database/migrations/2023_09_22_005613_create_to_do_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('to_dos', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('description')->nullable();
+            $table->enum('status',array('todo','active','completed'));
+            $table->unsignedBigInteger('user_id');
+            $table->timestamps();
+
+            $table->foreign('user_id')->on('users')->references('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('to_dos');
+    }
+};
